35371f127adf26fdbc19602741172a8be55525da
dungpham <me2015_dung.pq@student.vgu.edu.vn>
2021-08-20 22:58:43 +0700

    feature: add api client

--- services/apiClient.ts
+++ services/apiClient.ts
@@ -1,5 +1,35 @@
-class ApiClient{
+import authService from "./authService";
 
+const hey = {
+    "abc": "ok",
+    "def": "yes"
 }
 
 
+const apiClient = {
+    async fetch(url: string, body?: { [prop: string]: string }, method: string = 'get') {
+
+        const isPost = method === 'post'
+
+        if (isPost) {
+            return fetch(url, {
+                method,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${authService.key}`
+                },
+                body: JSON.stringify(body)
+            })
+        } else {
+            return fetch(url, {
+                method,
+                headers: {
+                    "Authorization": `Bearer ${authService.key}`
+                }
+            })
+        }
+
+    }
+}
+
+export default apiClient
