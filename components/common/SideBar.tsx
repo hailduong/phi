2a83ba8fa6cca961ba3609974d44b0dc0172611f
Ly Duong <hailyduong@gmail.com>
2021-10-31 12:12:52 +0700

    feat: update theme

--- components/common/SideBar.tsx
+++ components/common/SideBar.tsx
@@ -42,7 +42,7 @@ const SideBar = () => {
                         <div className="dropdown profile-element">
                                         <span data-toggle="dropdown" className="dropdown-toggle">
                                             <span
-                                                className="block m-t-xs font-bold">{doctorInfo?.firstName} {doctorInfo?.lastName}</span>
+                                                className="block m-t-xs font-bold text-white">{doctorInfo?.firstName} {doctorInfo?.lastName}</span>
                                             <span className="text-muted text-xs block">{doctorInfo?.role}
                                                 <b className="caret"/>
                                                 </span>
@@ -53,9 +53,9 @@ const SideBar = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div className="logo-element">
-                            PHI
-                        </div>
+                        {/*<div className="logo-element">*/}
+                        {/*    <Image src={'/img/logo.png'}/>*/}
+                        {/*</div>*/}
                     </li>
                     <li className="active">
                         {
