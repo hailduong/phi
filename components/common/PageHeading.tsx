d025157293ac8cad7d96e80a5740e6a05d509a91
dungpham <me2015_dung.pq@student.vgu.edu.vn>
2021-11-16 21:12:59 +0700

    feature: NAVIGATION on page heading

--- components/common/PageHeading.tsx
+++ components/common/PageHeading.tsx
@@ -1,4 +1,6 @@
 import s from './PageHeading.module.scss'
+import {useIsAdmin} from "./SideBar";
+import Link from 'next/link'
 
 type TProps = {
     pageName: string
@@ -6,13 +8,14 @@ type TProps = {
 
 const PageHeading = (props: TProps) => {
     const {pageName} = props
+    const isAdmin = useIsAdmin()
     return (
         <div className="row wrapper border-bottom white-bg page-heading">
             <div className="col-sm-4">
                 <h2 className={s.pageName}>{pageName}</h2>
                 <ol className="breadcrumb">
                     <li className="breadcrumb-item">
-                        <a href="index.html">Home</a>
+                        {isAdmin ? <Link href="/doctors">Home</Link> : <Link href="/">Home</Link>}
                     </li>
                     <li className="breadcrumb-item active">
                         <strong>{pageName}</strong>
